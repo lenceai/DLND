9c462159f5f3b5f7e493f8fd26b8019c7f5d1b59
infobots <44482010+infobots@users.noreply.github.com>
2020-01-23 06:22:26 -0800

    20_01_23

--- intro-to-pytorch/intro_pytorch_wrk.py
+++ intro-to-pytorch/intro_pytorch_wrk.py
@@ -4,7 +4,9 @@ Created on Tue Jan 21 06:55:10 2020
 
 @author: datar
 """
-#%%
+
+#%% 
+### 1. Introduction to Deep Learning with PyTorch
 
 import torch
 
@@ -23,4 +25,90 @@ features = torch.randn((1, 5))
 weights = torch.randn_like(features)
 
 bias = torch.randn((1, 1))
+
+#%%
+
+y = activation(torch.sum(features * weights) + bias)
+#y = activation((features * weights).sum() + bias)
+
+#%%
+
+y = activation(torch.mm(features, weights.view(5, 1)) + bias)
+
+#%%
+
+## Generate data
+torch.manual_seed(7)
+
+# Features are 3 random variables
+features = torch.randn((1, 3))
+
+# Define the size of each layter
+
+n_input = features.shape[1]
+n_hidden = 2
+n_output = 1
+
+# Weights
+W1 = torch.randn(n_input, n_hidden)
+W2 = torch.randn(n_hidden, n_output)
+
+# bias
+
+B1 = torch.randn((1, n_hidden))
+B2 = torch.randn((1, n_output))
+
+#%%
+
+h = activation(torch.mm(features, W1) + B1)
+output = activation(torch.mm(h, W2) + B2)
+print(output)
+
+
+#%%
+
+import numpy as np
+a = np.random.rand(4, 3)
+
+b = torch.from_numpy(a)
+
+c = b.numpy()
+
+b.mul_(2)
+
+#%% 
+###  2. Neural Networks with PyTorch
+
+#import matplotlib
+#config InlineBackend.figure_format = 'retina'
+
+import numpy as np
+import torch
+
+import helper
+
+import matplotlib.pyplot as plt
+
+from torchvision import datasets, transforms
+
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,)),])
+
+trainset = datasets.MNIST('C:/DLND/intro-to-pytorch/MNIST_data/', download=True, train=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+
+#%%
+
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+print(type(images))
+print(images.shape)
+print(labels.shape)
+
+plt.imshow(images[1].numpy().squeeze(), cmap='Greys_r');
+
+
+
+
+
+
 
